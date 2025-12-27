70ce9b71763bc2769e3581e02feefdbc004af7f3
meibohuhu <meibohu96@gmail.com>
2025-12-26 18:04:14 -0800

    from 122240

--- scripts/cluster_eval/internvl_eva_scripts/1219_121640/evaluate_internvl2_5_how2sign_2xa6000_0_121640.sh
+++ scripts/cluster_eval/internvl_eva_scripts/1219_121640/evaluate_internvl2_5_how2sign_2xa6000_0_121640.sh
@@ -26,7 +26,7 @@ NUM_DEVICES=$(echo "$GPU_IDS" | tr ',' '\n' | wc -l)
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-}"  # Empty by default - will use base model
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/local1/mhu/sign_language_llm/InternVL/checkpoints/finetune_internvl2_5_openasl_16fps_1130/checkpoint-8499}"
 
-CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/script_adobe/checkpoints/finetune_internvl2_5_how2sign_2b_1223_unfreeze12/checkpoint-2548}"
+CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/script_adobe/checkpoints/finetune_internvl2_5_how2sign_2b_1224_unfreeze12/checkpoint-3058}"
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/script_adobe/checkpoints/finetune_internvl2_5_how2sign_1b_16fps_1220/checkpoint-2548}"
 
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/local1/mhu/sign_language_llm/InternVL/checkpoints/finetune_internvl2_5_how2sign_16fps_1130/checkpoint-2399}"
@@ -48,7 +48,7 @@ OUT_DIR="${OUT_DIR:-/code/doc_sign_search/outputs/internvl_eval/}"
 
 # Evaluation parameters
 # MAX_SAMPLES=${MAX_SAMPLES:-20}  # Set to a number to limit samples, empty for full evaluation
-MAX_SAMPLES=${MAX_SAMPLES:-550}
+MAX_SAMPLES=${MAX_SAMPLES:-55}
 MIN_NUM_FRAMES=${MIN_NUM_FRAMES:-32}  # Minimum number of frames (set to 6 to ensure 6 frames)
 MAX_NUM_FRAMES=${MAX_NUM_FRAMES:-130}  # Maximum number of frames (set to 6 to fix at 6 frames)
 SAMPLING_METHOD=${SAMPLING_METHOD:-fps16.0}  # Sampling method: 'fpsX.X' or 'uniform' for uniform sampling
