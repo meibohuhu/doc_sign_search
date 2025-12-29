70adbb7518b3d390fbd60a661d6e9b69bbc6c05b
meibohuhu <meibohu96@gmail.com>
2025-12-28 16:39:36 -0800

    from 122230

--- scripts/cluster_eval/internvl_eva_scripts/1219_121630/evaluate_internvl2_5_how2sign_2xa6000_2_121630.sh
+++ scripts/cluster_eval/internvl_eva_scripts/1219_121630/evaluate_internvl2_5_how2sign_2xa6000_2_121630.sh
@@ -23,11 +23,11 @@ NUM_DEVICES=$(echo "$GPU_IDS" | tr ',' '\n' | wc -l)
 # Configuration
 # Set CHECKPOINT_PATH to empty string or unset to use base model only
 # Update checkpoint path to point to your trained checkpoint (or leave empty for base model)
-# CHECKPOINT_PATH="${CHECKPOINT_PATH:-}"  # Empty by default - will use base model
+CHECKPOINT_PATH="${CHECKPOINT_PATH:-}"  # Empty by default - will use base model
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/local1/mhu/sign_language_llm/InternVL/checkpoints/finetune_internvl2_5_openasl_16fps_1130/checkpoint-8499}"
 
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/script_adobe/checkpoints/finetune_internvl2_5_how2sign_8b_16fps_1209/checkpoint-2548}"
-CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/script_adobe/checkpoints/finetune_internvl2_5_how2sign_2b_1227_sample15000_notag/checkpoint-2976}"  ## 2744
+# CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/InternVL/checkpoints/finetune_internvl2_5_how2sign_2b_1227_sample2500_tag/checkpoint-2744}"  ## 2744
 
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/local1/mhu/sign_language_llm/InternVL/checkpoints/finetune_internvl2_5_how2sign_16fps_1130/checkpoint-2399}"
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/local1/mhu/sign_language_llm/InternVL/checkpoints/finetune_internvl2_5_how2sign_20fps/checkpoint-2874}"
@@ -35,22 +35,22 @@ CHECKPOINT_PATH="${CHECKPOINT_PATH:-/code/doc_sign_search/script_adobe/checkpoin
 # CHECKPOINT_PATH="${CHECKPOINT_PATH:-/local1/mhu/sign_language_llm/InternVL/checkpoints/finetune_internvl2_5_how2sign_20fps_freeze/checkpoint-2874}"
 
 
-MODEL_BASE="${MODEL_BASE:-OpenGVLab/InternVL2_5-8B}"
+MODEL_BASE="${MODEL_BASE:-OpenGVLab/InternVL2_5-2B}"
 # MODEL_BASE="${MODEL_BASE:-OpenGVLab/InternVL2_5-1B}"
 # VIDEO_FOLDER="${VIDEO_FOLDER:-}"
 VIDEO_FOLDER="${VIDEO_FOLDER:-/mnt/localssd/doc_sign_search/how2sign_test_videos_224x224}"
 
-# QUESTION_FILE="${QUESTION_FILE:-/code/doc_sign_search/InternVL/data/how2sign/test_how2sign_internvl.jsonl}"
+QUESTION_FILE="${QUESTION_FILE:-/code/doc_sign_search/InternVL/data/how2sign/test_how2sign_internvl.jsonl}"
 # QUESTION_FILE="${QUESTION_FILE:-/local1/mhu/sign_language_llm/InternVL/data/openasl/test_openasl_internvl.jsonl}"
-QUESTION_FILE="${QUESTION_FILE:-/code/doc_sign_search/InternVL/data/how2sign/test_how2sign_internvl_sample550.jsonl}"
+# QUESTION_FILE="${QUESTION_FILE:-/code/doc_sign_search/InternVL/data/how2sign/test_how2sign_internvl_sample550.jsonl}"
 
 OUT_DIR="${OUT_DIR:-/code/doc_sign_search/outputs/internvl_eval/}"
 
 # Evaluation parameters
 # MAX_SAMPLES=${MAX_SAMPLES:-20}  # Set to a number to limit samples, empty for full evaluation
-MAX_SAMPLES=${MAX_SAMPLES:-55}
+MAX_SAMPLES=${MAX_SAMPLES:-2337}
 MIN_NUM_FRAMES=${MIN_NUM_FRAMES:-32}  # Minimum number of frames (set to 6 to ensure 6 frames)
-MAX_NUM_FRAMES=${MAX_NUM_FRAMES:-150}  # Maximum number of frames (set to 6 to fix at 6 frames)
+MAX_NUM_FRAMES=${MAX_NUM_FRAMES:-130}  # Maximum number of frames (set to 6 to fix at 6 frames)
 SAMPLING_METHOD=${SAMPLING_METHOD:-fps16.0}  # Sampling method: 'fpsX.X' or 'uniform' for uniform sampling
 IMAGE_SIZE=${IMAGE_SIZE:-224}
 MAX_NEW_TOKENS=${MAX_NEW_TOKENS:-128}
@@ -160,7 +160,7 @@ else
 fi
 echo ""
 
-$CONDA_PYTHON scripts/cluster_eval/internvl_eva_scripts/internvl_evaluation_how2sign.py \
+$CONDA_PYTHON scripts/cluster_eval/internvl_eva_scripts/internvl_evaluation_how2sign_nogate.py \
     "${EVAL_ARGS[@]}" \
     2>&1 | tee "$LOG_FILE"
 
